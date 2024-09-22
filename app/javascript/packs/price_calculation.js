e1e73e1dbe1b6753084bc2eb515d5f4504191f62
aruga-eri <eri0725.t@gmail.com>
2024-09-23 06:47:20 +0900

    商品一覧表示絹の設定

---
+++ app/javascript/packs/price_calculation.js
@@ -0,0 +1,22 @@
+const price = () => {
+  const priceInput = document.getElementById("item-price");
+  if (priceInput) {
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      const addTaxDom = document.getElementById("add-tax-price");
+      const ProfitDom = document.getElementById("profit");
+
+      // 販売手数料の計算
+      const taxAmount = Math.floor(inputValue * 0.1);
+      addTaxDom.innerHTML = taxAmount;
+
+      // 利益の計算
+      ProfitDom.innerHTML = inputValue - taxAmount;
+    });
+  } else {
+    console.error("Element with ID 'item-price' not found.");
+  }
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
